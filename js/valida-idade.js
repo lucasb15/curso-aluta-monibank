d26d9dc3deaf93989c59f4351067c27fd2a0e48c
Lucas Gomes Santos <lgsantos306@gmail.com>
2023-06-06 21:00:19 -0300

    comecando-aula05

--- js/valida-idade.js
+++ js/valida-idade.js
@@ -1,6 +1,8 @@
 export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    console.log(validaIdade(dataNascimento));
+    if(!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('O usuário não e maior de idade')
+    }
 }
 
 function validaIdade(data) {
